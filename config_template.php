b5cbc419686b83fafbc27ec754bf4e1da5733038
Fabian <gitcommit@qnote.de>
2019-12-29 13:59:32 +0100

    License

--- config_template.php
+++ config_template.php
@@ -1,4 +1,10 @@
 <?php
+/*GPL
+ * This file is part of the T2 toolbox;
+ * Copyright (C) 2014-2020 Fabian Perder (t2@qnote.de) and contributors
+ * T2 comes with ABSOLUTELY NO WARRANTY. This is free software, and you are welcome to redistribute it under
+ * certain conditions. See the GNU General Public License (file 'LICENSE' in the root directory) for more details.
+ GPL*/
 /**TPLDOCSTART
  * This template is used by the installer wizard to create the configuration file "config_exclude.php".
  * @see \service\Install_wizard::prompt_dbParams()
--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
+/*GPL
+ * This file is part of the T2 toolbox;
+ * Copyright (C) 2014-2020 Fabian Perder (t2@qnote.de) and contributors
+ * T2 comes with ABSOLUTELY NO WARRANTY. This is free software, and you are welcome to redistribute it under
+ * certain conditions. See the GNU General Public License (file 'LICENSE' in the root directory) for more details.
+ GPL*/
+
 
 require_once 'Start.php';
 $page = \core\Page::init("PAGE_ID_TETHYSINDEX", "T2");
